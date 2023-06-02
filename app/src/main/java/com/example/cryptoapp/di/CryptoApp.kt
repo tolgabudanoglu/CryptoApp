44574a3ae5ec5181ff1e42c4dd0903517a408723
tolga budanoğlu <budanoglu.tolga@gmail.com>
2023-06-02 22:29:28 +0300

    create network Module

---
+++ app/src/main/java/com/example/cryptoapp/di/CryptoApp.kt
@@ -0,0 +1,9 @@
+package com.example.cryptoapp.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class CryptoApp: Application() {
+}
--- app/src/main/java/com/example/cryptoapp/utils/Constant.kt
+++ app/src/main/java/com/example/cryptoapp/utils/Constant.kt
@@ -1,4 +1,7 @@
 package com.example.cryptoapp.utils
 
-class Constant {
+object Constant {
+    const val BASE_URL = "https://pro-api.coinmarketcap.com"
+    const val API_KEY = "91cdf43d-4b1f-462d-8f72-2c5ecdc4bc64"
+    const val LIMIT = "100"
 }
